4b8c37b0cbabb3b04ee5d1b62bf29406d3a6005a
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-02-26 22:53:25 +0100

    Async and Await

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 console.log("Before");
 
-getUser(1)
-  .then((user) => getRepositories(user.gitHubUsername))
-  .then((repos) => getCommits(repos[0]))
-  .then((commits) => console.log("Commits", commits))
-  .catch((err) => console.log("Error", err.message));
+// getUser(1)
+//   .then((user) => getRepositories(user.gitHubUsername))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log("Commits", commits))
+//   .catch((err) => console.log("Error", err.message));
+
+// Async and Await
+async function displayCommits() {
+  const user = await getUser(1);
+  const repos = await getRepositories(user.gitHubUsername);
+  const commits = await getCommits(repos[0]);
+  console.log(commits);
+}
+displayCommits();
 
 console.log("After");
 
