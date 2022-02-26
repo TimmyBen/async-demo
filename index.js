fa84e7ca6b540f1a1df67323006a7b6be7c13dc5
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-02-26 20:52:24 +0100

    Async Op with Callbacks2

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log("Before");
 getUser(1, function (user) {
   console.log("User", user);
+
+  getRepositories(user.gitHubUsername, (repos) => {
+    console.log("Repos", repos);
+  });
 });
 console.log("After");
 
@@ -8,5 +12,12 @@ function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from a database...");
     callback({ id: id, gitHubUsername: "mosh" });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("Calling GitHub API...");
+    callback(["repo1", "repo2", "repo3"]);
   }, 2000);
 }
